390f5b5f3fb9d4f507d76f8cfff6514b8559f5ec
Marcelo Buongiorno <mnbuongiorno@gmail.com>
2023-10-04 20:13:48 -0300

    tercer ejercicio

---
+++ script.js
@@ -0,0 +1,18 @@
+function move(){
+    let image = document.querySelector(".astronauta")
+    image.style.marginLeft+="700px"
+}
+move();
+
+function achicar(){
+    let image = document.querySelector(".astronauta")
+    image.style.transform= "scale(0.6)"
+}
+achicar();
+
+function agrandar(){
+    let image = document.querySelector(".astronauta")
+    image.style.transform= "scale(1.2)"
+}
+agrandar(); 
+
