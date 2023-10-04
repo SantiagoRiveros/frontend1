fca05bb1b74bf15aa3ff07f27d050f5be3487c76
Santiago Riveros Jasis <70020219+SantiagoRiveros@users.noreply.github.com>
2023-10-04 19:53:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ let image = document.querySelector(".astronauta")
 image.style.marginLeft+="700px";
 }
 move()
+
+function peque (){
+    let imapeq = document.querySelector(".astro")
+    imapeq.style.transform = "scale(0.8)"
+   
+}
+peque();
