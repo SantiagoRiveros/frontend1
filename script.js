3f2c6ef0f85b9b762468f67cebbf6968230cb02c
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2023-10-04 20:34:20 -0300

    ultima tarea chiquita

--- script.js
+++ script.js
@@ -15,32 +15,38 @@ function Animate() {
 }
 Animate() */
 
-function move(){
-    let img = document.querySelector(".astro")
-    img.style.marginLeft+="700px"
+function move() {
+  let img = document.querySelector(".astro");
+  img.style.marginLeft += "700px";
 }
-move()
+move();
 
-function reduce(){
-    let img = document.querySelector(".astro")
-    img.style.transform="scale(0.6)"
+function reduce() {
+  let img = document.querySelector(".astro");
+  img.style.transform = "scale(0.6)";
 }
-reduce()
+reduce();
 
-function enhance(){
-    let img = document.querySelector(".astro")
-    img.style.transform="scale(1.2)"
+function enhance() {
+  let img = document.querySelector(".astro");
+  img.style.transform = "scale(1.2)";
 }
-enhance()
+enhance();
 
-function resetPosition(){
-    let img = document.querySelector(".astro")
-    img.style.marginLeft="30px"
+function resetPosition() {
+  let img = document.querySelector(".astro");
+  img.style.marginLeft = "30px";
 }
-resetPosition()
+resetPosition();
 
-function MoveUp(){
-    let img = document.querySelector(".astro")
-    img.style.marginTop="10px"
+function MoveUp() {
+  let img = document.querySelector(".astro");
+  img.style.marginTop = "10px";
 }
-MoveUp()
+MoveUp();
+
+function cambiarFondo() {
+  let space = document.querySelector(".main-container");
+  space.style.backgroundColor = "#420a8d";
+}
+cambiarFondo();
