cf3ae14a09bda97cbaa327a0975b67ddfe76c513
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2023-10-04 20:21:21 -0300

    cuarto ejercicio

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-function move(){
-    let image = document.querySelector(".astronauta")
-    image.style.marginLeft+="700px"
+function move() {
+  let image = document.querySelector(".astronauta");
+  image.style.marginLeft += "700px";
 }
 move();
 
-function achicar(){
-    let image = document.querySelector(".astronauta")
-    image.style.transform= "scale(0.6)"
+function achicar() {
+  let image = document.querySelector(".astronauta");
+  image.style.transform = "scale(0.6)";
 }
 achicar();
 
-function agrandar(){
-    let image = document.querySelector(".astronauta")
-    image.style.transform= "scale(1.2)"
+function agrandar() {
+  let image = document.querySelector(".astronauta");
+  image.style.transform = "scale(1.2)";
 }
-agrandar(); 
+agrandar();
 
+function volverAlInicio() {
+  let image = document.querySelector(".astronauta");
+  image.style.marginLeft = "30px";
+  image.style.transform = "scale(1)";
+}
+volverAlInicio();
