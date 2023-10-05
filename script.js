f63b3bb2c2bb5fdf8f6ba81a4350b0e886054302
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2023-10-04 21:02:55 -0300

    fadjofao

--- script.js
+++ script.js
@@ -21,36 +21,47 @@ let space = document.querySelector(".main-container");
 let botonColor = document.querySelector(".button-color");
 
 function move() {
-  img.style.marginLeft += "700px";
+  let resultado = parseInt(astronauta.style.marginLeft) + 700;
+  astronauta.style.marginLeft = resultado + "px";
 }
 
 function reduce() {
-  img.style.transform = "scale(0.6)";
+  astronauta.style.transform = "scale(0.6)";
 }
 
 function enhance() {
-  img.style.transform = "scale(1.2)";
+  astronauta.style.transform = "scale(1.2)";
 }
 
 function resetPosition() {
-  img.style.marginLeft = "30px";
-  img.style.marginTop = "120px";
+  astronauta.style.marginLeft = "30px";
+  astronauta.style.marginTop = "120px";
 }
 
 function MoveUp() {
-  img.style.marginTop = "10px";
+  astronauta.style.marginTop = "10px";
 }
 
 function cambiarFondo() {
   space.style.backgroundColor = "#420a8d";
 }
 
-botonColor.addEventListener("click", function () {
-  cambiarFondo();
-});
-
+/* FUNCIONES COMPUESTAS */
 function posicionInicialAgrandar() {
   resetPosition();
   enhance();
 }
 posicionInicialAgrandar();
+
+/* EVENT LISTENERS */
+botonColor.addEventListener("click", function () {
+  cambiarFondo();
+});
+
+document.addEventListener("keydown", (event) => {
+  console.log(event.code);
+  if (event.code === "Space") {
+    move();
+    reduce();
+  }
+});
